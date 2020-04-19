ce813927e1bef3b5273bfc8bf3e5c6b912ff4161
Maheen <Maheen@maheenskpro2015.lan>
2020-04-20 03:12:38 +0800

    Feat : Find Peak Algo

---
+++ challenges/Chapter_5_Binary_Search/Find_Peak/findPeak.js
@@ -0,0 +1,27 @@
+/**
+A = [1,3,4,5,2] => Peak = 5
+A = [5,3,1] => Peak = 5
+A = [1,3,5] => Peak = 5
+ */
+export function findPeak(arr = []) {
+  if (arr.length === 0) {
+    return null;
+  }
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    const mid = start + Math.floor((end - start) / 2);
+    const left = mid === 0 ? Number.NEGATIVE_INFINITY : arr[mid - 1];
+    const right =
+      mid === arr.length - 1 ? Number.NEGATIVE_INFINITY : arr[mid + 1];
+
+    if (arr[mid] > left && arr[mid] > right) {
+      return mid;
+    } else if (arr[mid] > left && arr[mid] < right) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+}
---
+++ challenges/Chapter_5_Binary_Search/Find_Peak/findPeak.spec.js
@@ -0,0 +1,26 @@
+import { findPeak } from './findPeak';
+
+test('should return the correct peak for arr of size 5', () => {
+  const arr = [1, 3, 4, 5, 2];
+  expect(findPeak(arr)).toBe(3);
+});
+
+test('should return the correct peak for arr of size 4', () => {
+  const arr = [1, 3, 4, 5];
+  expect(findPeak(arr)).toBe(3);
+});
+
+test('should return the correct peak for arr of size 4', () => {
+  const arr = [5, 1, 3];
+  expect(findPeak(arr)).toBe(0);
+});
+
+test('should return null if arr is []', () => {
+  const arr = [];
+  expect(findPeak(arr)).toBe(null);
+});
+
+test('should the correct peak if arr length is 1', () => {
+  const arr = [1];
+  expect(findPeak(arr)).toBe(0);
+});
