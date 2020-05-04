8503117ac513b70f3a109fafd85aa93c640dfa7b
Maheen <Maheen@maheenskpro2015.lan>
2020-05-04 12:59:29 +0800

    Feat : Doubly Linked List Implementation

---
+++ challenges/Chapter_7_LinkedList/Doubly_Linked_List/Implementation/doubllyLinkedList.js
@@ -0,0 +1,44 @@
+export class Node {
+  constructor(data, next = null, prev = null) {
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+export class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(data) {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return;
+  }
+
+  removeTail() {
+    if (!this.head) {
+      return;
+    }
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+  }
+}
---
+++ challenges/Chapter_7_LinkedList/Doubly_Linked_List/Implementation/doubllyLinkedList.spec.js
@@ -0,0 +1,63 @@
+import { DoublyLinkedList, Node } from './doubllyLinkedList';
+
+describe('Doubly Linked List Implementation should work', () => {
+  test('List is a class', () => {
+    expect(typeof DoublyLinkedList.prototype.constructor).toEqual('function');
+  });
+
+  test('Node is a class', () => {
+    expect(typeof Node.prototype.constructor).toEqual('function');
+  });
+
+  test('has properties "data" , "next" and "prev"', () => {
+    const node = new Node('a', 'b', 'c');
+    expect(node.data).toEqual('a');
+    expect(node.next).toEqual('b');
+    expect(node.prev).toEqual('c');
+  });
+
+  test('append function works with empty list', () => {
+    const dll = new DoublyLinkedList();
+    dll.append(1);
+    expect(dll.head.data).toBe(1);
+  });
+
+  test('append function works with non-empty list', () => {
+    const dll = new DoublyLinkedList();
+    dll.append(1);
+    dll.append(3);
+    dll.append(2);
+    expect(dll.tail.data).toBe(2);
+    expect(dll.tail.prev.data).toBe(3);
+    expect(dll.head.next.data).toBe(3);
+    expect(dll.head.data).toBe(1);
+    expect(dll.length).toBe(3);
+  });
+
+  test('removeLast function works with empty list', () => {
+    const dll = new DoublyLinkedList();
+    dll.removeTail();
+    expect(dll.head).toBe(null);
+    expect(dll.tail).toBe(null);
+    expect(dll.length).toBe(0);
+  });
+
+  test('removeLast function works with list length is 1', () => {
+    const dll = new DoublyLinkedList();
+    dll.append(1);
+    dll.removeTail();
+    expect(dll.head).toBe(null);
+    expect(dll.tail).toBe(null);
+    expect(dll.length).toBe(0);
+  });
+
+  test('removeLast function works with non-empty list', () => {
+    const dll = new DoublyLinkedList();
+    dll.append(1);
+    dll.append(3);
+    dll.append(2);
+    dll.removeTail();
+    expect(dll.tail.data).toBe(3);
+    expect(dll.tail.next).toBe(null);
+  });
+});
