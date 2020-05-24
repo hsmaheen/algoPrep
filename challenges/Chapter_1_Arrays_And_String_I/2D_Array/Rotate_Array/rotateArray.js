1441598fd74025d251b7f6369109a5a1b433d210
Maheen <Maheen@maheenskpro2015.lan>
2020-05-24 15:21:39 +0800

    Chore : update variables

--- challenges/Chapter_1_Arrays_And_String_I/2D_Array/Rotate_Array/rotateArray.js
+++ challenges/Chapter_1_Arrays_And_String_I/2D_Array/Rotate_Array/rotateArray.js
@@ -1,23 +1,23 @@
-export function rotateArray(matrix) {
-  if (matrix === null) {
+export function rotateArray(arr) {
+  if (arr === null) {
     return null;
   }
 
-  const layers = Math.floor(matrix.length / 2);
-  const size = matrix.length - 1;
+  const layers = Math.floor(arr.length / 2);
+  const size = arr.length - 1;
   for (let layer = 0; layer < layers; layer++) {
     for (let j = layer; j < size - layer; j++) {
-      const top = matrix[layer][j];
-      const right = matrix[j][size - layer];
-      const bottom = matrix[size - layer][size - j];
-      const left = matrix[size - j][layer];
+      const top = arr[layer][j];
+      const right = arr[j][size - layer];
+      const bottom = arr[size - layer][size - j];
+      const left = arr[size - j][layer];
 
-      matrix[layer][j] = left;
-      matrix[j][size - layer] = top;
-      matrix[size - layer][size - j] = right;
-      matrix[size - j][layer] = bottom;
+      arr[layer][j] = left;
+      arr[j][size - layer] = top;
+      arr[size - layer][size - j] = right;
+      arr[size - j][layer] = bottom;
     }
   }
 
-  return matrix;
+  return arr;
 }
