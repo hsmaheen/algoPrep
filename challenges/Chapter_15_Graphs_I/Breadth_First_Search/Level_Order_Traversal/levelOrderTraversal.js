46965218081201c3b7ed5902275684b4b9bd6b17
CoderFin <coderfin@pop-os.localdomain>
2020-07-31 22:28:28 +0800

    Feat : Level Order Traversal

---
+++ challenges/Chapter_15_Graphs_I/Breadth_First_Search/Level_Order_Traversal/levelOrderTraversal.js
@@ -0,0 +1,37 @@
+import { Queue } from '../../../Chapter_9_Queue/Implementation/queueImplementation';
+import { Graph } from '../../Implementation/graph';
+
+export function levelOrderTraversal(graph) {
+  const levels = [];
+
+  if (graph.nodes.size > 0) {
+    const rootNode = graph.nodes.values().next().value;
+
+    traverseNode(rootNode, levels, graph);
+  }
+
+  return levels;
+}
+
+function traverseNode(node, levels, graph) {
+  const processQueue = new Queue();
+  processQueue.enqueue(node);
+
+  while (!processQueue.isEmpty()) {
+    const currentLevel = [];
+
+    let layerSize = processQueue.length;
+
+    while (layerSize > 0) {
+      const currNode = processQueue.dequeue();
+      currentLevel.push(currNode.val);
+      for (const edge of currNode.edges.values()) {
+        const edgeNode = graph.getNode(edge);
+        processQueue.enqueue(edgeNode);
+      }
+      layerSize--;
+    }
+
+    levels.push(currentLevel);
+  }
+}
